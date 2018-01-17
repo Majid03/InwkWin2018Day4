9698e6114912495895a74654d64d2f1401f27502
majid03 <majid@dal.ca>
2018-01-17 13:57:41 +0000

    Wrote task 1

---
+++ task1.py
@@ -0,0 +1,18 @@
+import math
+
+#The functions that are asked
+def my_square(s):
+    """ Returns the area of a square given side s"""
+    return S*S
+
+def my_circle(r):
+    return(math.pi*r*r)
+
+
+def my_sphere(r):
+    return((4/3)*r*my_Circle(r))
+
+x = input("Enter the value for square")
+x = int(x)
+
+print ("The area of square is " + str(my_square(x)))
